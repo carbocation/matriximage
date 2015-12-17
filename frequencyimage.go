3182e355d4856a446bfa47f14754b5098bfc2a25
Carbocation <james@carbocation.com>
2015-12-17 13:38:15 -0500

    Permit masks to be applied.

--- frequencyimage.go
+++ frequencyimage.go
@@ -1,12 +1,60 @@
 package matriximage
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/mjibson/go-dsp/dsputils"
 	"github.com/mjibson/go-dsp/fft"
 )
 
 type FourierImage struct {
 	*dsputils.Matrix // Is expected to be the result of a Fourier transform
+}
+
+// Generate a new FourierImage by applying a mask to the current one
+// The mask is assumed to have its frequency origin at the center, which
+// is shifted by N/2 and M/2 from the top left origin
+func (f FourierImage) ApplyImageMask(img *Image) (*FourierImage, error) {
+	mask := img.toGrayMatrix()
+
+	return f.ApplyMatrixMask(mask)
+}
+
+func (f FourierImage) ApplyMatrixMask(mask *dsputils.Matrix) (*FourierImage, error) {
+	dims := f.Dimensions()
+	maskDims := mask.Dimensions()
+
+	if len(dims) != len(maskDims) {
+		return nil, fmt.Errorf("Mask had different number of dimensions from image")
+	}
+	for i := 0; i < len(dims); i++ {
+		if dims[i] != maskDims[i] {
+			return nil, fmt.Errorf("Size of mask dimension %d (%d) differs from size of image dimension %d (%d)", i, maskDims[i], i, dims[i])
+		}
+	}
+
+	m := dsputils.MakeEmptyMatrix(dims)
+
+	// While the mask has been shifted with respect to the orientation of the matrix,
+	// the internal matrix representation has not. So, we pull the mask value from
+	// a shifted value, and pull the matrix value from an unshifted value.
+	for y := 0; y < dims[0]; y++ {
+		for x := 0; x < dims[1]; x++ {
+			shiftedY, shiftedX := (y+dims[0]/2)%dims[0], (x+dims[1]/2)%dims[1]
+
+			v := f.Value([]int{y, x})
+			maskV := mask.Value([]int{shiftedY, shiftedX})
+
+			// Multiply the value at this pixel by a factor from 0-1, depending on
+			// the mask's amplitude compared to the max possible amplitude
+			product := v * complex((real(maskV)/math.MaxUint8), 0)
+
+			m.SetValue(product, []int{y, x})
+		}
+	}
+
+	return &FourierImage{Matrix: m}, nil
 }
 
 // For making masks
--- matriximage_test.go
+++ matriximage_test.go
@@ -2,11 +2,48 @@ package matriximage
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"testing"
+
+	"github.com/mjibson/go-dsp/dsputils"
 )
 
-const Case = "churchill"
+const Case = "rand"
+
+func TestMask(t *testing.T) {
+	m, err := FromFile(fmt.Sprintf("./images/%s.gif", Case))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fourier := m.DFT()
+
+	maskMatrix := dsputils.MakeEmptyMatrix(fourier.Dimensions()) //fourier.AmplitudeImage().toGrayMatrix()
+	dims := maskMatrix.Dimensions()
+
+	// Create a square mask, where only the middle part of the amplitude is recovered.
+	for y := 0; y < dims[0]; y++ {
+		for x := 0; x < dims[1]; x++ {
+			//if y < 62*dims[0]/128 || x < 62*dims[1]/128 || y > 65*dims[0]/128 || x > 65*dims[1]/128 {
+			if (y >= dims[0]/2-2 && y <= dims[0]/2+2) && (x >= dims[1]/2-2 && x <= 1+dims[1]/2+2) {
+				fmt.Println(y, x)
+				maskMatrix.SetValue(complex(0, 0), []int{y, x})
+			} else {
+				maskMatrix.SetValue(complex(float64(math.MaxUint8), 0), []int{y, x})
+			}
+		}
+	}
+
+	maskedFourier, err := fourier.ApplyMatrixMask(maskMatrix)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testSave(*maskedFourier.AmplitudeImage(), "masked-amplitude")
+	testSave(*maskedFourier.BrighterAmplitudeImage(), "masked-brighter-amplitude")
+	testSave(*maskedFourier.IDFTImage(), "masked-recovered")
+}
 
 func TestFourierImage(t *testing.T) {
 	m, err := FromFile(fmt.Sprintf("./images/%s.gif", Case))
